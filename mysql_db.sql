e7691df6ddf3577f22c2c9e4860eb74f80fc3e64
yura <itmailbox@i.ua>
2017-02-06 01:44:20 +0200

    fix mysql file

--- mysql_db.sql
+++ mysql_db.sql
@@ -5,16 +5,7 @@ CREATE TABLE companies
     quota BIGINT(20) NOT NULL
 );
 CREATE UNIQUE INDEX companies_name_uindex ON companies (name);
-CREATE TABLE transfers
-(
-    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    date DATETIME NOT NULL,
-    user_id INT(11) NOT NULL,
-    resource TEXT NOT NULL,
-    transferred BIGINT(20),
-    CONSTRAINT transfers_users_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE INDEX transfers_users_id_fk ON transfers (user_id);
+
 CREATE TABLE users
 (
     id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -23,3 +14,13 @@ CREATE TABLE users
     company_id INT(11) NOT NULL
 );
 CREATE INDEX users_companies_id_fk ON users (company_id);
+CREATE TABLE transfers
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  date DATETIME NOT NULL,
+  user_id INT(11) NOT NULL,
+  resource TEXT NOT NULL,
+  transferred BIGINT(20),
+  CONSTRAINT transfers_users_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE INDEX transfers_users_id_fk ON transfers (user_id);
